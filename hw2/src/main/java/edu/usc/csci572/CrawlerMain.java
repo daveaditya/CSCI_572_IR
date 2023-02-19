5d75a9abe27ca5ad0aab44903adb8a93a1e9cb12
Aditya Dave <connect@adityadave.dev>
2023-02-19 15:22:41 -0800

    implement Crawler4J example; ignore data directory

--- hw2/src/main/java/edu/usc/csci572/CrawlerMain.java
+++ hw2/src/main/java/edu/usc/csci572/CrawlerMain.java
@@ -1,8 +1,39 @@
 package edu.usc.csci572;
 
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
 public class CrawlerMain {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String crawlStorageFolder = "./data/crawl/root";
+        int numberOfCrawlers = 7;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        // Instantiate the controller for this crawl.
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        // For each crawl, you need to add some seed urls. These are the first
+        // URLs that are fetched and then the crawler starts following links
+        // which are found in these pages
+        controller.addSeed("https://www.ics.uci.edu/~lopes/");
+        controller.addSeed("https://www.ics.uci.edu/~welling/");
+        controller.addSeed("https://www.ics.uci.edu/");
+
+        // The factory which creates instances of crawlers.
+        CrawlController.WebCrawlerFactory<WebCrawler> factory = MyCrawler::new;
+
+        // Start the crawl. This is a blocking operation, meaning that your code
+        // will reach the line after this only when crawling is finished.
+        controller.start(factory, numberOfCrawlers);
     }
 
 }
