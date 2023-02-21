321d245097490b77b4a0165f7c9f96e371b80d2a
Aditya Dave <connect@adityadave.dev>
2023-02-21 01:19:32 -0800

    add Crawler implementation with support for stats logging

---
+++ hw2/src/main/java/edu/usc/csci572/Main.java
@@ -0,0 +1,93 @@
+package edu.usc.csci572;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    @Parameter(names = {"--seed-url", "-u"}, description = "URL of the website to crawl.")
+    private String seedUrl = "https://www.usatoday.com/";
+
+    @Parameter(names = {"--max-pages", "-mp"}, description = "The maximum number of pages to crawl.")
+    private int maxPages = 20000;
+
+    @Parameter(names = {"--max-depth", "-d"}, description = "The maximum depth to crawl to.")
+    private int maxDepth = 16;
+
+    @Parameter(names = {"--num-crawlers", "-nt"}, description = "Number of crawlers/thread to create.")
+    private int numberOfCrawlers = 8;
+
+    @Parameter(names = {"--politeness", "-p"}, description = "The politeness delay specifies to number of milliseconds"
+            + "to wait between requests.")
+    private int politenessDelay = 2000;
+
+    @Parameter(names = {"--output-dir", "-od"}, description = "Directory to store the statistics in.")
+    private String outputDirectory = "submission/";
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+
+        JCommander.newBuilder()
+                .addObject(main)
+                .build()
+                .parse(args);
+        main.run();
+    }
+
+    public void run() throws Exception {
+        logger.debug("Seed URL: {}", seedUrl);
+        logger.debug("Max Pages: {}", maxPages);
+        logger.debug("Max Depth: {}", maxDepth);
+        logger.debug("Number of Crawlers: {}", numberOfCrawlers);
+        logger.debug("Politeness Delay: {} ms", politenessDelay);
+
+        // TODO: validate seed URL
+
+        String crawlStorageFolder = "results";
+        int numberOfCrawlers = this.numberOfCrawlers;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+        config.setMaxPagesToFetch(maxPages);
+        config.setPolitenessDelay(politenessDelay);
+        config.setMaxDepthOfCrawling(maxDepth);
+
+        // Instantiate the controller for this crawl.
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        // For each crawl, you need to add some seed urls. These are the first
+        // URLs that are fetched and then the crawler starts following links
+        // which are found in these pages
+        controller.addSeed(seedUrl);
+
+        // The factory which creates instances of crawlers.
+        CrawlStats crawlStats = CrawlStats.getInstance();
+        CrawlController.WebCrawlerFactory<MyCrawler> factory = () -> new MyCrawler(seedUrl, crawlStats);
+
+        // Start the crawl. This is a blocking operation, meaning that your code
+        // will reach the line after this only when crawling is finished.
+        controller.start(factory, numberOfCrawlers);
+
+        // NOTE: No need to aggregate thread results as CrawlStats is Singleton and thread-safe
+
+        // Get domain name
+        String domain = seedUrl.replaceAll("http(s)?://|www\\.|/.*", "").toLowerCase();
+
+        // Store Stats
+        Utils.writeStats(outputDirectory, domain, crawlStats);
+
+        logger.debug("Done.");
+    }
+
+}
--- hw2/src/main/java/edu/usc/csci572/MyCrawler.java
+++ hw2/src/main/java/edu/usc/csci572/MyCrawler.java
@@ -4,14 +4,31 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+import edu.usc.csci572.beans.Fetch;
+import edu.usc.csci572.beans.Url;
+import edu.usc.csci572.beans.Visit;
+import org.apache.http.impl.EnglishReasonPhraseCatalog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Locale;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 public class MyCrawler extends WebCrawler {
 
-    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
-            + "|png|mp3|mp4|zip|gz))$");
+    private static final Logger logger = LoggerFactory.getLogger(MyCrawler.class);
+
+    private final String orgWebsite;
+
+    private final CrawlStats stats;
+
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(html|doc|docx|pdf|bmp|gif|jpeg|jpg|png|svg))$");
+
+    public MyCrawler(String orgWebsite, CrawlStats stats) {
+        this.orgWebsite = orgWebsite;
+        this.stats = stats;
+    }
 
     /**
      * This method receives two parameters. The first parameter is the page
@@ -20,14 +37,22 @@ public class MyCrawler extends WebCrawler {
      * the given url should be crawled or not (based on your crawling logic).
      * In this example, we are instructing the crawler to ignore urls that
      * have css, js, git, ... extensions and to only accept urls that start
-     * with "https://www.ics.uci.edu/". In this case, we didn't need the
+     * with "orgWebsite". In this case, we didn't need the
      * referringPage parameter to make the decision.
      */
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
         String href = url.getURL().toLowerCase();
-        return !FILTERS.matcher(href).matches()
-                && href.startsWith("https://www.ics.uci.edu/");
+        boolean residesInside = href.startsWith(this.orgWebsite);
+
+        // Store all the URLs checked or visited and also mention whether it is within the website or not
+        this.stats.addUrl(new Url(
+                url.getDocid(),
+                url.getURL(),
+                residesInside ? Url.ResidesWithinWebsite.OK : Url.ResidesWithinWebsite.N_OK
+        ));
+
+        return FILTERS.matcher(href).matches() && residesInside;
     }
 
     /**
@@ -36,18 +61,54 @@ public class MyCrawler extends WebCrawler {
      */
     @Override
     public void visit(Page page) {
+        int docid = page.getWebURL().getDocid();
         String url = page.getWebURL().getURL();
-        System.out.println("URL: " + url);
+        int statusCode = page.getStatusCode();
+        String statusMessage = EnglishReasonPhraseCatalog.INSTANCE.getReason(statusCode, Locale.ENGLISH);
 
-        if (page.getParseData() instanceof HtmlParseData) {
-            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-            String text = htmlParseData.getText();
-            String html = htmlParseData.getHtml();
-            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+        this.stats.incStatusCodeCounts(String.format("%d %s", statusCode, statusMessage.toUpperCase())); // Update status code count stat
 
-            System.out.println("Text length: " + text.length());
-            System.out.println("Html length: " + html.length());
-            System.out.println("Number of outgoing links: " + links.size());
+        this.stats.addFetch(new Fetch(docid, url, statusCode)); // record new fetch
+
+        this.stats.incNumOfFetches(); // increment number of fetches
+        this.stats.incTotalUrls(); // increment total visited urls
+
+        if(statusCode == 200) {
+            this.stats.incNumOfSuccessfulFetches();             // Increment Successful Visit Count
+
+            Visit visit = new Visit(); // create a new Visit
+            visit.setDocid(docid);
+            visit.setUrl(url);
+
+            if (page.getParseData() instanceof HtmlParseData htmlParseData) {
+                Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+                visit.setNumOfOutlinks(links.size()); // Store number of outlinks
+            }
+
+            // Add content type for current fetch; avoid charset=utf-8
+            String contentType = page.getContentType().split(";")[0];
+            visit.setContentType(contentType);
+
+            this.stats.addContentTypeCount(contentType); // count Content-Type occurrences
+
+            // Add content length for current fetch
+            int size = page.getContentData().length;
+            visit.setSize(size);
+
+            this.stats.addFileSizeCount(size); // count Size range
+
+            this.stats.addVisit(visit); // add Visit to the stats
+        } else {
+            this.stats.incNumOfFailedFetches(); // Increment failed/aborted visit counts
         }
+
+        // TODO: Save data based on frequency like after every 50 pages
     }
+
+    @Override
+    public Object getMyLocalData() {
+        return this.stats;
+    }
+
 }
