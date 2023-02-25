0fe7c2da044557641cd2c5c5420341d772f8291f
Aditya Dave <connect@adityadave.dev>
2023-02-25 12:52:32 -0800

    add build step to run script

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,1 +1,2 @@
-java $JVM_OPTIONS -cp "${PWD}/build/libs/*" edu.usc.csci572.Main "$@"
+./gradlew clean build # build the jar
+java $JVM_OPTIONS -cp "${PWD}/build/libs/*" edu.usc.csci572.Main "$@" # run the jar file
